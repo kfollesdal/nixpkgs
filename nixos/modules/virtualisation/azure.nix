23c276ab860fc16e8d74b1041cc76ae62bc19a33
Kristoffer Føllesdal <kfollesdal@gmail.com>
2022-08-18 19:18:36 +0200

    _save

--- nixos/modules/virtualisation/azure.nix
+++ nixos/modules/virtualisation/azure.nix
@@ -22,7 +22,7 @@ with lib;
     boot.kernelParams = [
       # Kernel parameters recommende for Azure [1] and [3]
       "rootdelay=300"
-      "console=tty1"
+      # "console=tty1"
       "console=ttyS0,115200" # [3]
       "earlyprintk=ttyS0"
       "net.ifnames=0"

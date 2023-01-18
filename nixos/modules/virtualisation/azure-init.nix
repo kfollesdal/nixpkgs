e15138ac762e08b2209067b5f84ab9462a36aa4e
kfollesdal <kristoffer.follesdal@eviny.no>
2023-01-18 18:23:07 +0100

    Print out metadata

--- nixos/modules/virtualisation/azure-init.nix
+++ nixos/modules/virtualisation/azure-init.nix
@@ -6,8 +6,10 @@ signal-ready = pkgs.writeShellApplication {
   name = "signal-ready";
   runtimeInputs = with pkgs; [ curl ];
   text = ''
-    #ls -a /metadata # TEMPE REMOVE
-    #ls -a /metadata2 # TEMP REMOVE
+    ls -a /metadata # TEMPE REMOVE
+    ls -a /metadata2 # TEMP REMOVE
+
+    cat /metadata2/ovf-env.xml && cat /metadata/ovf-env.xml
 
     attempts=1
     until [ "$attempts" -gt 5 ]
